19ab369b648b1c4fb546890b11e99dad95d60cd6
finlaypearce <finlaypearce1@gmail.com>
2018-03-15 20:26:45 +0000

    Add practice table

--- NEA/models.py
+++ NEA/models.py
@@ -1,6 +1,7 @@
 from NEA import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     access = db.Column(db.Integer())
+    practice = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,3 +25,15 @@ class User(UserMixin, db.Model):
 #@login_manager.user_loader
 #def load_user(id):
   #  return User.query.get(int(id))
+
+
+class Practice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    duration = db.Column(db.Integer)
+    instrument = db.Column(db.String(32))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Practice {}>'.format(self.body)
